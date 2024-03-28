69e966b9e3cc3f51f3e419eb7f5e9142fa58220c
joel <joellf10@hotmail.com>
2024-03-28 15:49:09 +0100

    Se implementa la estructura base del PriceService y se crea el endpoint en PriceController "/price" con los parametros de entrada que representan la "fecha de aplicación", "identificador de producto" y "identificador de cadena"

--- pricing-service/src/main/java/com/capitole/pricingservice/domain/adapter/rest/PriceController.java
+++ pricing-service/src/main/java/com/capitole/pricingservice/domain/adapter/rest/PriceController.java
@@ -1,5 +1,34 @@
 package com.capitole.pricingservice.domain.adapter.rest;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capitole.pricingservice.domain.application.dto.PriceResponseDTO;
+import com.capitole.pricingservice.service.PriceService;
+
+@RestController
 public class PriceController {
 
+	private final PriceService priceService;
+
+    @Autowired
+    public PriceController(PriceService priceService) {
+        this.priceService = priceService;
+    }
+    
+    @GetMapping("/price")
+    public ResponseEntity<PriceResponseDTO> getPrice(
+            @RequestParam("requestDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime requestDate,
+            @RequestParam("productId") Long productId,
+            @RequestParam("brandId") Long brandId
+    ) {
+        return ResponseEntity.ok(priceService.getPrice(requestDate, productId, brandId));
+    }
+    
 }
--- pricing-service/src/main/java/com/capitole/pricingservice/service/PriceService.java
+++ pricing-service/src/main/java/com/capitole/pricingservice/service/PriceService.java
@@ -1,5 +1,26 @@
 package com.capitole.pricingservice.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.capitole.pricingservice.domain.application.dto.PriceResponseDTO;
+import com.capitole.pricingservice.domain.repository.PriceRepository;
+
+@Service
 public class PriceService {
 
+    private final PriceRepository priceRepository;
+
+    @Autowired
+    public PriceService(PriceRepository priceRepository) {
+        this.priceRepository = priceRepository;
+    }
+    
+    public PriceResponseDTO getPrice(LocalDateTime applicationDate, Long productId, Long brandId) {
+
+        return null;
+    }    
+    
 }
