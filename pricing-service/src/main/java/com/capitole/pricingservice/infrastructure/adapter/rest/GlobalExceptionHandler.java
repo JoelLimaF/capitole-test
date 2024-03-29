afb12fb1bf5f8d9bf81a2a726dad14b45e1abacd
joel <joellf10@hotmail.com>
2024-03-29 16:07:30 +0100

    Se añaden nuevos ExeceptionHandler para personalizar las excepciones del constructor de parametro nulo o de formato incorrecto

--- pricing-service/src/main/java/com/capitole/pricingservice/infrastructure/adapter/rest/GlobalExceptionHandler.java
+++ pricing-service/src/main/java/com/capitole/pricingservice/infrastructure/adapter/rest/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package com.capitole.pricingservice.infrastructure.adapter.rest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.capitole.pricingservice.application.exception.PriceNotFoundException;
 
@@ -14,4 +16,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handlePriceNotFoundException(PriceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
+	
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<String> handleMissingParams(MissingServletRequestParameterException ex) {
+        String name = ex.getParameterName();
+        return new ResponseEntity<>("The parameter '" + name + "' is required", HttpStatus.BAD_REQUEST);
+    }
+	
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+	            .body("Incorrect parameter: " + ex.getMessage());
+	}
+
 }
